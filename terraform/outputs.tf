5ea3c607b8a995d8550a627d15fd97fdab5ee198
Gokul D <mgokuldeepak@gmail.com>
2024-08-18 17:30:18 +0530

    Cosmetic change to the terraform outputs.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
-output "mlflow_server_public_ip" {
-  value = module.mlflow.mlflow_server_public_ip
+output "mlflow_server_url" {
+  value = "http://${module.mlflow.mlflow_server_public_ip}:5000"
 }
 
-output "airflow_server_public_ip" {
-  value = module.airflow.airflow_server_public_ip
+output "airflow_server_url" {
+  value = "http://${module.airflow.airflow_server_public_ip}:8080"
 }
