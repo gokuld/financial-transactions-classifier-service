b2ad6bc904e32dfd861af15cf9722608060bea2f
Gokul D <mgokuldeepak@gmail.com>
2024-08-20 10:52:25 +0530

    Set default values for some terraform variables.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,11 +17,6 @@ variable "mlflow_artifact_store_s3_bucket_name" {
   type = string
 }
 
-variable "airflow_dags_local_path" {
-  description = "Local path where the Airflow DAG scripts are stored. This will be copied to the Airflow instance."
-  type        = string
-}
-
 variable "dataset_bucket_name" {
   description = "Name of the bucket where the dataset is stored."
   type        = string
@@ -30,9 +25,17 @@ variable "dataset_bucket_name" {
 variable "dataset_parquet_file_bucket_key" {
   description = "Key of the dataset file in the S3 bucket"
   type        = string
+  default     = "data/description-category-data-sample.parquet"
+}
+
+variable "airflow_dags_local_path" {
+  description = "Local path where the Airflow DAG scripts are stored. This will be copied to the Airflow instance."
+  type        = string
+  default     = "../pipelines/airflow/dags/"
 }
 
 variable "bentoml_service_source_local_path" {
   description = "Local path containing the source to build and run the bentoml model service. This will be copied to the model service instance."
   type        = string
+  default     = "../model_service/"
 }
